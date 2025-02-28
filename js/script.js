7142841aea38debe62f39f0774c991c7be3ee4fc
Viktor <example@gmail.com>
2025-02-28 23:07:34 +0200

    second tast

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 "use strict"
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
